d2429f21d43318f6872787470c85cf60bd8342af
sairammanem <79075350+sairammanem@users.noreply.github.com>
2021-04-07 10:29:37 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,68 @@
+setTimeout(() => {
+
+
+    let a = 10;
+    console.log(a)
+}, 1000);
+
+setTimeout(() => {
+
+
+    let a = 9;
+    console.log(a)
+}, 2000);
+setTimeout(() => {
+
+
+    let a = 8;
+    console.log(a)
+}, 3000);
+setTimeout(() => {
+
+
+    let a = 7;
+    console.log(a)
+}, 4000);
+
+setTimeout(() => {
+
+
+    let a = 6;
+    console.log(a)
+}, 5000);
+setTimeout(() => {
+
+
+    let a = 5;
+    console.log(a)
+}, 6000);
+setTimeout(() => {
+
+
+    let a = 4;
+    console.log(a)
+}, 7000);
+setTimeout(() => {
+
+
+    let a = 3;
+    console.log(a)
+}, 8000);
+setTimeout(() => {
+
+
+    let a = 2;
+    console.log(a)
+}, 9000);
+setTimeout(() => {
+
+
+    let a = 1;
+    console.log(a)
+}, 10000);
+setTimeout(() => {
+
+
+
+    console.log('Happy Independence Day')
+}, 11000);
